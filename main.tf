7bafeb27d284c69e1809d01d3b5ca77e4c937f6c
gkranasinghe <gkranasinghe@gmail.com>
2022-04-07 00:25:46 +0530

    corrected ipv4.address values from lxd

--- main.tf
+++ main.tf
@@ -1,10 +1,5 @@
-# locals {
-#   nodes  =  [for n in range(var.worker_count) : "${lxd_container.worker[n].ipv4_address} is ${n}"]
-  
-
-# }
-
 resource "lxd_container" "master" {
+  
   count = var.master_count
   name  = "master${count.index}"
   
@@ -30,18 +25,23 @@ resource "lxd_container" "master" {
   limits = {
     cpu = 2
   }
+
+
   provisioner "local-exec" {
 
 
     working_dir = var.ansible_dir
-    command     = "ansible-playbook   playbook.yaml"
-    # on_failure = fail
+    command     = " ansible-playbook -i ${tolist(self.device)[0].properties["ipv4.address"]}  playbook.yaml  "
+   # on_failure = fail
   }
 
 
 }
 
 resource "lxd_container" "worker" {
+depends_on = [
+  lxd_container.master
+]
   count = var.worker_count
   name  = "worker${count.index}"
   
@@ -67,18 +67,55 @@ resource "lxd_container" "worker" {
   limits = {
     cpu = 2
   }
+
   provisioner "local-exec" {
 
 
     working_dir = var.ansible_dir
-    command     = "ansible-playbook   playbook.yaml"
-    # on_failure = fail
+    command     = " ansible-playbook -i ${tolist(self.device)[0].properties["ipv4.address"]}  playbook.yaml  "
+    on_failure = fail
   }
 
 
 }
 
+resource "lxd_container" "nfsserver" {
+  count = var.master_count
+  name  = "nfsserver${count.index}"
+  
+  image     = "ubuntu:20.04"
+  ephemeral = false
+  profiles  = ["${lxd_profile.profile1.name}"]
 
+  config = {
+    "boot.autostart" = true
+  }
+  device {
+    name = "eth0"
+    type = "nic"
+
+    properties = {
+      nictype        = "bridged"
+      parent         = "${lxd_network.new_default.name}"
+      "ipv4.address" = "10.150.19.3${count.index}"
+    }
+  }
+
+
+  limits = {
+    cpu = 1
+  }
+  provisioner "local-exec" {
+
+
+    working_dir = var.ansible_dir
+    command     = "ansible-playbook -i ${tolist(self.device)[0].properties["ipv4.address"]}  playbook.yaml"
+    # on_failure = fail
+  }
+
+
+
+}
 
 resource "local_file" "inventory" {
   filename = "ansible/inventory/hosts"
@@ -86,19 +123,24 @@ resource "local_file" "inventory" {
       content = <<EOT
 # inventory/hosts      
 [master_nodes]
-${lxd_container.master[0].name}    ip_address=${lxd_container.master[0].ipv4_address}
-
+${lxd_container.master[0].name}    ip_address=${tolist(lxd_container.master[0].device)[0].properties["ipv4.address"]}
 
 [worker_nodes]
-${lxd_container.worker[0].name}    ip_address=${lxd_container.worker[0].ipv4_address}
-${lxd_container.worker[1].name}    ip_address=${lxd_container.worker[1].ipv4_address}
+${lxd_container.worker[0].name}    ip_address=${tolist(lxd_container.worker[0].device)[0].properties["ipv4.address"]}
+${lxd_container.worker[1].name}    ip_address=${tolist(lxd_container.worker[1].device)[0].properties["ipv4.address"]}
+
+[nfs_nodes]
+${lxd_container.nfsserver[0].name}    ip_address=${lxd_container.nfsserver[0].ipv4_address}
+
+
 
 [all:vars]
 ansible_connection=lxd
 ansible_python_interpreter=/usr/bin/python3
-hosttempfile_location=/home/gk
+hosttempfile_location=~/
 
 EOT
 
    
 }
+
