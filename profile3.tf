e983c75edfa7d08888058421c72d3fb1aafb8f78
gkranasinghe <gkranasinghe@gmail.com>
2022-04-15 13:49:35 +0530

    add patch

--- profile3.tf
+++ profile3.tf
@@ -6,7 +6,7 @@ resource "lxd_profile" "profile3" {
     "limits.cpu"           = "4"
     "limits.memory"        = "6GB"
     "limits.memory.swap"   = "false"
-    "linux.kernel_modules" = "ip_tables,ip6_tables,nf_nat,overlay,br_netfilter,rbd"
+    "linux.kernel_modules" = "ip_tables,ip6_tables,nf_nat,overlay,br_netfilter,rbd,zfs"
     "raw.lxc"              = "lxc.mount.entry = /dev/kmsg dev/kmsg none defaults,bind,create=file\nlxc.apparmor.profile=unconfined\nlxc.cap.drop=\nlxc.cgroup.devices.allow=a\nlxc.mount.auto=proc:rw sys:rw"
     "security.nesting"     = "true"
     "security.privileged"  = "true"
